af74b8e61384cb1503f5b25cc7f9f638d80aa91d
Gwendal Gourmelon <ggourmelon@prodival.com>
2018-11-07 09:49:47 +0100

    first commit

---
+++ application/layout/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>StellarIsen</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="public/css/bootstrap/bootstrap.min.css">
+    <script src="public/js/jquery-3.3.1.min.js"></script>
+    <script src="public/js/popper.min.js"></script>
+    <script src="public/js/bootstrap/bootstrap.min.js"></script>
+
+    <script src="public/js/main.js"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="public/js/main.css" />
+</head>
+<body>
---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php require("../application/layout/header.php"); ?>
+<?php include("../application/layout/navbar.php") ?>
+plop
+<?php require("../application/layout/footer.php"); ?>
