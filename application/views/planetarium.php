07807203253bf66641bd51bc0ba68b3af1c98bb7
Gwendal Gourmelon <ggourmelon@prodival.com>
2018-11-15 16:47:48 +0100

    application des changement

--- application/views/planetarium.php
+++ application/views/planetarium.php
@@ -1,10 +1,101 @@
-
-
-  <iframe width="1400" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://virtualsky.lco.global/embed/?longitude=-119.86286000000001&latitude=34.4326&projection=gnomic&constellations=true&constellationlabels=true&constellationboundaries=true&meteorshowers=true&showstarlabels=true&showorbits=true&ecliptic=true&meridian=true&gridlines_az=true&gridlines_eq=true&gridlines_gal=true&showgalaxy=true&az=125.25855333708353" allowTransparency="true"></iframe>
-
- <!--
+<div class="contentPlanetarium">
+    <div class="sideBar">
+        <form class="choixProjection" action="">Choix de projection :
+            <br>
+            <input type="radio" name="choixProjection" value="polar"> Polar
+            <br>
+            <input type="radio" name="choixProjection" value="stereo"> Stereo
+            <br>
+            <input type="radio" name="choixProjection" value="lambert"> Lambert
+            <br>
+            <input type="radio" name="choixProjection" value="equirectangular"> Equirectangular
+            <br>
+            <input type="radio" name="choixProjection" value="mollweide"> Mollweide
+            <br>
+            <input type="radio" name="choixProjection" value="planechart"> Planechart
+            <br>
+            <input type="radio" name="choixProjection" value="gnomic"> Gnomic
+            <br>
+        </form>
+        <div class="zoneSwitchs">
+            <label class="switch">
+                <input type="checkbox" checked>
+                <span class="slider round"></span>
+            </label> Lignes des constellations
+            <br>
+            <label class="switch">
+                <input type="checkbox">
+                <span class="slider round"></span>
+            </label> Noms des constellations
+            <br>
+            <label class="switch">
+                <input type="checkbox">
+                <span class="slider round"></span>
+            </label> Limites des constellations
+            <br>
+            <label class="switch">
+                <input type="checkbox">
+                <span class="slider round"></span>
+            </label> Pluie de météorites
+            <br>
+            <label class="switch">
+                <input type="checkbox">
+                <span class="slider round"></span>
+            </label> Noms des étoiles
+            <br>
+            <label class="switch">
+                <input type="checkbox">
+                <span class="slider round"></span>
+            </label> Orbites
+            <br>
+            <label class="switch">
+                <input type="checkbox">
+                <span class="slider round"></span>
+            </label> Ecliptic
+            <br>
+            <label class="switch">
+                <input type="checkbox">
+                <span class="slider round"></span>
+            </label> Méridien
+            <br>
+            <label class="switch">
+                <input type="checkbox">
+                <span class="slider round"></span>
+            </label> Grille azimuthale
+            <br>
+            <label class="switch">
+                <input type="checkbox">
+                <span class="slider round"></span>
+            </label> Grille équatoriale
+            <br>
+            <label class="switch">
+                <input type="checkbox">
+                <span class="slider round"></span>
+            </label> Grille galactique
+            <br>
+            <label class="switch">
+                <input type="checkbox">
+                <span class="slider round"></span>
+            </label> Galaxie
+            <br>
+            <label class="switch">
+                <input type="checkbox">
+                <span class="slider round"></span>
+            </label> Ciel en live
+            <br>
+        </div>
+    </div>
+    <iframe id="sky" class="virtualSky" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://virtualsky.lco.global/embed/?longitude=-119.86286000000001&latitude=34.4326&projection=gnomic&constellations=true&constellationlabels=true&constellationboundaries=true&meteorshowers=true&showstarlabels=true&showorbits=true&ecliptic=true&meridian=true&gridlines_az=true&gridlines_eq=true&gridlines_gal=true&showgalaxy=true&az=125.25855333708353" allowTransparency="true"></iframe>
+    <button id="collapse-sidebar">Paramètres</button>
+</div>
+<script>
+    document.getElementById("collapse-sidebar").onclick = function(event) {
+        var vir = document.getElementById("sky");
+        vir.className = (vir.className == "virtualSky") ? "virtualSkyFull" : "virtualSky";
+    }
+</script>
+<!--
 "https://virtualsky.lco.global/embed/?
-
 
 longitude=-119.86286000000001&
 latitude=34.4326&
@@ -19,7 +110,8 @@ meteorshowers=true&
 showstarlabels=true&
 showorbits=true&
 ecliptic=true&
-meridian=true&
+meridian=
+true&
 gridlines_az=true&
 gridlines_eq=true&
 gridlines_gal=true&
@@ -28,72 +120,6 @@ background=rgba(255,255,255,0)&
 live=true&
 az=309.6831492755458"
 -->
-
-<form class="choixProjection" action="">Choix de projection :<br>
-  <input type="radio" name="choixProjection" value="polar"> Polar<br>
-  <input type="radio" name="choixProjection" value="stereo"> Stereo<br>
-  <input type="radio" name="choixProjection" value="lambert"> Lambert<br>
-  <input type="radio" name="choixProjection" value="equirectangular"> Equirectangular<br>
-  <input type="radio" name="choixProjection" value="mollweide"> Mollweide<br>
-  <input type="radio" name="choixProjection" value="planechart"> Planechart<br>
-  <input type="radio" name="choixProjection" value="gnomic"> Gnomic<br>
-</form>
-
-<div class="zoneSwitchs">
-<label class="switch">
-  <input type="checkbox" checked>
-  <span class="slider round"></span>
-</label> Lignes des constellations<br>
-<label class="switch">
-  <input type="checkbox">
-  <span class="slider round"></span>
-</label> Noms des constellations<br>
-<label class="switch">
-  <input type="checkbox">
-  <span class="slider round"></span>
-</label> Limites des constellations<br>
-<label class="switch">
-  <input type="checkbox">
-  <span class="slider round"></span>
-</label> Pluie de météorites<br>
-<label class="switch">
-  <input type="checkbox">
-  <span class="slider round"></span>
-</label> Noms des étoiles<br>
-<label class="switch">
-  <input type="checkbox">
-  <span class="slider round"></span>
-</label> Orbites<br>
-<label class="switch">
-  <input type="checkbox">
-  <span class="slider round"></span>
-</label> Ecliptic<br>
-<label class="switch">
-  <input type="checkbox">
-  <span class="slider round"></span>
-</label> Méridien<br>
-<label class="switch">
-  <input type="checkbox">
-  <span class="slider round"></span>
-</label> Grille azimuthale<br>
-<label class="switch">
-  <input type="checkbox">
-  <span class="slider round"></span>
-</label> Grille équatoriale<br>
-<label class="switch">
-  <input type="checkbox">
-  <span class="slider round"></span>
-</label> Grille galactique<br>
-<label class="switch">
-  <input type="checkbox">
-  <span class="slider round"></span>
-</label> Galaxie<br>
-<label class="switch">
-  <input type="checkbox">
-  <span class="slider round"></span>
-</label> Ciel en live<br>
-</div>
-
 
 <!--
 <label class="selectionAffichagePlanet">
@@ -149,5 +175,3 @@ az=309.6831492755458"
   <span class="checkmark"></span>
 </label><br>
 -->
-
-<?php require("../application/layout/footer.php"); ?>
