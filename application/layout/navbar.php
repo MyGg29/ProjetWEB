662af7f0ed16d30caec89657ea6e48a6c15489bb
Gwendal Gourmelon <ggourmelon@prodival.com>
2018-11-15 14:04:31 +0100

    resolve links 2

--- application/layout/navbar.php
+++ application/layout/navbar.php
@@ -22,7 +22,7 @@
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Rechercher..." aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="boutonRecherche">
-          <img src="../../public/icon/search.svg"> 
+          <img src="public/icon/search.svg"> 
       </button>
     </form>
      
