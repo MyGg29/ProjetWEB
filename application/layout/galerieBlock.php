43959c311b6545a49ff7252e23a214154960e70e
Gwendal Gourmelon <ggourmelon@prodival.com>
2018-11-30 20:04:12 +0100

    si legend est vide, on affiche pas le cadre

--- application/layout/galerieBlock.php
+++ application/layout/galerieBlock.php
@@ -1,5 +1,7 @@
 <?php //todo : $height is bad ?>
 <div class="block" >
   <img id="<?=$key?>" src="<?=$imgLink?>" alt="<?=$imgLink?>">
+  <?php if(!empty($legend)){?>
   <div class="legend"><?=$legend?></div>
+  <?php }?>
 </div>
