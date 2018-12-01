83c90bcfbe1d72ecfa7afe0e03d7bfe9f5b60e59
Quentin BECART <quentin.becart@isen.yncrea.fr>
2018-12-02 00:09:39 +0100

    ajout actus

--- application/controllers/indexController.php
+++ application/controllers/indexController.php
@@ -5,14 +5,14 @@ class IndexController{
         $this->titleBigNews = "NASA InSight sur Mars !";
         $this->contentBigNews = "Le module NASA InSight a pu prendre cette semaine une photo de la surface de Mars !<br>NASA Interior Exploration using Seismic Investigation, Geodesy and Heat Transport (InSight) va nous permettre de mieux comprendre la composition interne de Mars.";
 
-        $this->pathSmallImage1 = "public/img/jupiter.jpg";
-        $this->titleSmallNews1 = "Custom title 1";
+        $this->pathSmallImage1 = "public/img/kepler-planetes.jpg";
+        $this->titleSmallNews1 = "1/3 des exoplanètes seraient des planètes-océans";
 
-        $this->pathSmallImage2 = "public/img/jupiter.jpg";
-        $this->titleSmallNews2 = "Custom title 2";
+        $this->pathSmallImage2 = "public/img/super_imgburaco_negro_1_0.jpg";
+        $this->titleSmallNews2 = "Les trous noirs supermassifs sur le point d'entrer en collision sont nombreux";
 
-        $this->pathSmallImage3 = "public/img/jupiter.jpg";
-        $this->titleSmallNews3 = "Custom title 3";
+        $this->pathSmallImage3 = "public/img/psychelongshot0718b-586e.jpeg";
+        $this->titleSmallNews3 = "Nasa : feu vert à une mission d'exploration des astéroïdes troyens de Jupiter";
         require("../application/views/accueil.php");
     }
 }
