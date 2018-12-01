92da7c3607eca3bc51cc61974b7860173817ac4a
Gwendal Gourmelon <ggourmelon@prodival.com>
2018-12-01 15:13:40 +0100

    maj grosse actu

--- application/configs/configs.php
+++ application/configs/configs.php
@@ -1,13 +1,4 @@
 <?php
- 
-/*
-    The important thing to realize is that the config file should be included in every
-    page of your project, or at least any page you want access to these settings.
-    This allows you to confidently use these settings throughout a project because
-    if something changes such as your database credentials, or a path to a specific resource,
-    you'll only need to update it here.
-*/
- 
 $config = array(
     "db" => array(
             "dbname" => "stellarisen",
@@ -16,15 +7,8 @@ $config = array(
             "host" => "localhost"
     ),
     "urls" => array(
-        "baseUrl" => "http://example.com"
+        "baseUrl" => "localhost:8080"
     ),
-    "paths" => array(
-        "resources" => "/path/to/resources",
-        "images" => array(
-            "content" => $_SERVER["DOCUMENT_ROOT"] . "/images/content",
-            "layout" => $_SERVER["DOCUMENT_ROOT"] . "/images/layout"
-        )
-    )
 );
  
 ?>
--- application/controllers/indexController.php
+++ application/controllers/indexController.php
@@ -1,9 +1,9 @@
 <?php
 class IndexController{
     function show(){
-        $this->pathBigImage = "public/img/jupiter.jpg";
-        $this->titleBigNews = "Custom Title";
-        $this->contentBigNews = "Custom content for the news ! Fun fact and stuff";
+        $this->pathBigImage = "public/img/InSight.PNG";
+        $this->titleBigNews = "NASA InSight sur Mars !";
+        $this->contentBigNews = "Le module NASA InSight a pu prendre cette semaine une photo de la surface de Mars !<br>NASA Interior Exploration using Seismic Investigation, Geodesy and Heat Transport (InSight) va nous permettre de mieux comprendre la composition interne de Mars.";
 
         $this->pathSmallImage1 = "public/img/jupiter.jpg";
         $this->titleSmallNews1 = "Custom title 1";
